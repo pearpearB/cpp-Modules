d4e8ed5dd9de8d6622f12c2b485d109a97847f15
pearpearB <smilyoung@gmail.com>
2022-12-21 18:51:08 +0900

    [Feat] module00 ex00 구현

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jabae <jabae@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/21 18:15:39 by jabae             #+#    #+#             */
+/*   Updated: 2022/12/21 18:26:00 by jabae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 0; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			std::cout << (char)toupper(argv[i][j]); // static_cast<char> ??
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
