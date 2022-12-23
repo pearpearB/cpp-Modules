142c97850c8adfe8d5390b9e6b21b337ab773b04
pearpearB <smilyoung@gmail.com>
2022-12-23 18:29:50 +0900

    [Fix] Makefile 플래그 변경 및 42헤더 삭제

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -1,28 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jabae <jabae@student.42seoul.kr>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/21 18:15:39 by jabae             #+#    #+#             */
-/*   Updated: 2022/12/21 18:55:47 by jabae            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-	if (argc == 1)
-	{
+int main(int argc, char **argv) {
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (int i = 1; i < argc; i++)
-	{
-		for (int j = 0; argv[i][j]; j++)
-		{
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; argv[i][j]; j++) {
 			std::cout << (char)toupper(argv[i][j]); // static_cast<char> ??
 		}
 	}
