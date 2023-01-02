b5f947de9036736e68bc6bd077e95e295c4d7408
pearpearB <smilyoung@gmail.com>
2023-01-02 17:08:34 +0900

    [Feat] ex02 구현

---
+++ Module01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void) {
+	std::string stringVAR = "HI THIS IS BRAIN";
+	std::string *stringPTR = &stringVAR;
+	std::string &stringREF = stringVAR;
+
+	// memory address
+	std::cout << "stringVAR: " << &stringVAR << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	// value
+	std::cout << "stringVAR: " << stringVAR << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+	return 0;
+}
