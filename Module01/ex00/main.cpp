06f998c42aaea79b4582cbd1da6d4176fe9a88e8
pearpearB <smilyoung@gmail.com>
2023-01-04 19:33:39 +0900

    [Feat] 힙, 스택 스코프 공부 주석

--- Module01/ex00/main.cpp
+++ Module01/ex00/main.cpp
@@ -3,7 +3,7 @@
 void makeHeapZ(void) {
 	Zombie* newZ = newZombie("heapZ");
 	newZ->announce();
-	delete newZ;
+	delete newZ; // delete로 할당 해제를 안 하면 메모리 누수가 발생함
 }
 
 int main(void) {
@@ -19,3 +19,12 @@ int main(void) {
 
 	return 0;
 }
+
+/*
+defaultZ: BraiiiiiiinnnzzzZ...
+stackZ: BraiiiiiiinnnzzzZ...
+stackZ is dead									// stackZ를 선언한 randomChump()가 끝나면 소멸자가 불러짐
+heapZ: BraiiiiiiinnnzzzZ...
+heapZ is dead										// heapZ를 선언한 makeHeapZ()가 끝나면 소멸자가 불러짐
+defaultZ is dead								// defaultZ를 선언한 main문이 끝나면 소멸자가 불러짐
+*/
