3654509935fa51584464d73e50b32e2cfe373e07
pearpearB <smilyoung@gmail.com>
2023-01-05 13:43:55 +0900

    [Refactor] 안 쓰는 헤더, 함수 순서 정리

--- Module01/ex00/Zombie.cpp
+++ Module01/ex00/Zombie.cpp
@@ -4,9 +4,9 @@ void Zombie::announce(void) {
 	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
-Zombie::Zombie(std::string name): _name(name) {}
-
 Zombie::Zombie(): _name("defaultZ") {}
+
+Zombie::Zombie(std::string name): _name(name) {}
 
 Zombie::~Zombie() {
 	std::cout << this->_name << " is dead" << std::endl;
--- Module01/ex00/Zombie.hpp
+++ Module01/ex00/Zombie.hpp
@@ -1,7 +1,6 @@
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
-# include <string>
 # include <iostream>
 
 class Zombie {
