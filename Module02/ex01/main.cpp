3cc0e5087c91437d53c6735c8327c8b1b053db12
pearpearB <smilyoung@gmail.com>
2023-01-08 17:51:48 +0900

    [Feat] toInt, toFloat 공부하기

--- Module02/ex01/main.cpp
+++ Module02/ex01/main.cpp
@@ -20,3 +20,14 @@ int main(void) {
 
 	return 0; 
 }
+
+/*
+a is 1234.43 // roundf로 반올림해서 뒤를 버리고 int형으로 저장한 뒤, toFloat()하면 1234.43이 나온다.
+b is 10
+c is 42.4219 // 256을 곱하고 올림한 뒤, toFloat()으로 256을 나누면 42.421875...이 나온다.
+d is 10
+a is 1234 as integer
+b is 10 as integer
+c is 42 as integer
+d is 10 as integer
+*/
