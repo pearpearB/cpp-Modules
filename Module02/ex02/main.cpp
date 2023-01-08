35826d91cf542f4a7eec54f738c0f38f1f76e67a
pearpearB <smilyoung@gmail.com>
2023-01-08 18:26:31 +0900

    [Delete] 서브젝트에 없는 테스트 삭제

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -3,8 +3,6 @@
 int main(void) { 
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	Fixed c(10);
-	Fixed d(10.0f);
 
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
@@ -15,8 +13,6 @@ int main(void) {
 	std::cout << b << std::endl;
 
 	std::cout << Fixed::max( a, b ) << std::endl;
-
-	std::cout << (c == d) << std::endl;
 
 	return 0; 
 }
