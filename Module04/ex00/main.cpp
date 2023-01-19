b119c6d9bce153978b613c482229de36c6fd3bf4
pearpearB <smilyoung@gmail.com>
2023-01-19 13:53:51 +0900

    [Feat] 릭, 상속 설명을 위한 test case 추가

--- Module04/ex00/main.cpp
+++ Module04/ex00/main.cpp
@@ -2,31 +2,66 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
+void testLeaks() {
+	{
+		std::cout << "\n<-----test each class----->" << std::endl;
+		const Animal* animal = new Animal();
+		const Cat* cat = new Cat();
+		const Dog* dog = new Dog();
+
+		std::cout << animal->getType() << std::endl;
+		std::cout << cat->getType() << std::endl;
+		std::cout << dog->getType() << std::endl;
+		animal->makeSound();
+		cat->makeSound();
+		dog->makeSound();
+
+		delete animal;
+		delete cat;
+		delete dog;
+	}
+	{
+		std::cout << "\n<-----test parent class----->" << std::endl;
+		const Animal* animal = new Animal();
+
+		delete animal;       // 자식 클래스를 가리키기 전에, 릭을 방지할 수 있도록 delete 해주기!
+
+		animal = new Cat();  // 부모 클래스는 자식 클래스를 가리킬 수 있다.
+		animal->makeSound();
+
+		delete animal;
+
+		animal = new Dog();
+		animal->makeSound();
+		
+		delete animal;
+	}
+	{
+		std::cout << "\n<-----test const----->" << std::endl;
+		const Animal* animal = new Animal();
+		const Animal* cat = new Cat();
+
+		*animal = *cat; // error: cannot assign to variable 'animal' with const-qualified type 'const Animal *const'
+	}
+}
+
 int main() {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
-    meta->makeSound();
+	testLeaks();
+	system("leaks animal");
 
-	delete meta;
-	delete j;
-	delete i;
+	{
+		std::cout << "\n<-----test not use virtual----->" << std::endl;
+		const WrongAnimal* wrong = new WrongAnimal();
+		const WrongAnimal* wrongCat = new WrongCat();
 
-	std::cout << std::endl;
+		wrong->makeSound();
+		wrongCat->makeSound();
 
-	const WrongAnimal* wrong = new WrongAnimal();
-	const WrongAnimal* wrongCat = new WrongCat();
+		delete wrong;
+		delete wrongCat;
 
-    wrong->makeSound();
-	wrongCat->makeSound();
-
-	delete wrong;
-	delete wrongCat;
+	}
 
 	return 0;
 }
