32bf64b1af33809aa512e6ef1f5215a432b462c1
pearpearB <smilyoung@gmail.com>
2023-01-19 14:31:29 +0900

    [Feat] 설명 추가

--- Module04/ex01/main.cpp
+++ Module04/ex01/main.cpp
@@ -2,20 +2,20 @@
 #include "Dog.hpp"
 
 int main() {
-	// {
-	// 	Animal *animals[4] = {new Dog(), new Dog(), new Cat(), new Cat()};
-
-	// 	for (int i = 0; i < 4; i++)
-	// 		std::cout << animals[i]->getType() << std::endl;
-
-	// 	for (int i = 0; i < 4; i++)
-	// 		delete animals[i];
-	// }
-	// std::cout << std::endl;
 	{
-		Dog	*dog = new Dog();
-		Dog	cloneDog(*dog);
-		delete dog;
+		Animal *animals[4] = {new Dog(), new Dog(), new Cat(), new Cat()};
+
+		for (int i = 0; i < 4; i++)
+			std::cout << animals[i]->getType() << std::endl;
+
+		for (int i = 0; i < 4; i++)
+			delete animals[i];
+	}
+	std::cout << std::endl;
+	{
+		Dog  *dog1 = new Dog();
+		Dog dog2 = *dog1;
+		delete dog1; // 얕은 복사면 같은 포인터를 해제해 터지게 되는데, 깊은 복사로 인해 해제가 안되어 터지지 않는다.
 	}
 
 	return 0;
