684cae7f15d68e8da8af6127ba6aa361159c91f9
pearpearB <smilyoung@gmail.com>
2023-01-19 16:05:46 +0900

    [Style] 난잡한 주석 정리

--- Module04/ex01/Cat.cpp
+++ Module04/ex01/Cat.cpp
@@ -6,8 +6,7 @@ Cat::Cat(): Animal(), _brain(new Brain()) {
 	std::cout << "Cat is born" << std::endl;
 }
 
-Cat::Cat(const Cat &src): Animal(src), _brain(new Brain()) { // _brain(new Brain(*src._brain)) ?
-	// this->_brain = new Brain();
+Cat::Cat(const Cat &src): Animal(src), _brain(new Brain()) { // _brain(new Brain(*src._brain)) 도 가능, Brain의 copy constructor가 깊은 복사를 하기 때문
 	*this = src;
 	std::cout << "Copy cat is born by copy constructor" << std::endl;
 }
@@ -20,11 +19,8 @@ Cat::~Cat() {
 Cat &Cat::operator=(const Cat &rhs) {
 	if (this != &rhs) {
 		this->_type = rhs._type;
-		*this->_brain = *rhs._brain; // 조건? NULL이 아니면이 필요할까? 깊은복사로 만들어주기!
-		// *(this->_brain) = *(rhs._brain); //[1]    55487 bus error  ./animal 
-		// this->_brain = rhs._brain; 		// [1]    55654 abort      ./animal
-											// malloc: *** error for object 0x7fa9de00b600: pointer being freed was not allocated
-		// this->_brain = new Brain(*rhs._brain); // 얕은 복사
+		*this->_brain = *rhs._brain; // 깊은 복사
+		// this->_brain = rhs._brain; // 얕은 복사 // 이렇게 하게 되면, main의 두번째 case delete에서 터지게 된다. (dog1과 dog2가 같은 주소를 공유하고 있으므로, 해제한 곳을 또 해제해서)
 	}
 	return *this;
 }
