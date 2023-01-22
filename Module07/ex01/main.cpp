58c4e929b98aa5b495fce70dc4b9e3e506061289
pearpearB <smilyoung@gmail.com>
2023-01-22 19:46:19 +0900

    [Feat] ex01 구현 완료

---
+++ Module07/ex01/main.cpp
@@ -0,0 +1,14 @@
+#include "iter.hpp"
+#include <iostream>
+
+void print(int const &i) {
+	std::cout << i << std::endl;
+}
+
+int main(void) {
+	int array[] = {1, 2, 3, 4, 5};
+
+	iter(array, 5, print);
+
+	return 0;
+}
