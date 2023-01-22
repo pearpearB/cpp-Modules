cd68bebc831ded0e19d8b82e29fdb81239012980
pearpearB <smilyoung@gmail.com>
2023-01-22 18:47:38 +0900

    [Study] numeric_limits<char>::min() -128이었음...

--- Module06/ex00/test.cpp
+++ Module06/ex00/test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main() {
+void studyStrtodF() {
 	char	*end;
 
 	std::cout << "\n===== study strtod & tset nan =====" << std::endl;
@@ -19,17 +19,32 @@ int main() {
 	double	d3 = std::strtod("inf", NULL);  // check inf;
 	std::cout << "return value: " << d3 << std::endl;
 	std::cout << "compare infinity(): " << (d3 == std::numeric_limits<double>::infinity() ? "true" : "false") << std::endl;
+}
 
+void studyNumericLimitsF() {
 	std::cout << "\n===== study numeric_limits =====" << std::endl;
+	std::cout << "double" << std::endl;
 	std::cout << "max: " << std::numeric_limits<double>::max() << "\n" 
 	<< "min: " << std::numeric_limits<double>::min() << "\n" // 중간값, 0
 	<< "lowest: " << std::numeric_limits<double>::lowest() << std::endl;
 	std::cout << "compare lowest & -max(): " << (std::numeric_limits<double>::lowest() == -std::numeric_limits<double>::max() ? "true" : "false") << std::endl;
 
+	std::cout << "float" << std::endl;
 	std::cout << "max: " << std::numeric_limits<float>::max() << "\n" 
 	<< "min: " << std::numeric_limits<float>::min() << "\n" // 중간값, 0
 	<< "lowest: " << std::numeric_limits<float>::lowest() << std::endl;
 	std::cout << "compare lowest & -max(): " << (std::numeric_limits<float>::lowest() == -std::numeric_limits<float>::max() ? "true" : "false") << std::endl;
+
+	std::cout << "char" << std::endl;
+	std::cout << "max: " << (int)std::numeric_limits<char>::max() << "\n"
+	<< "min: " << (int)std::numeric_limits<char>::min() << "\n"
+	<< "u_min: " << (int)std::numeric_limits<u_char>::min() << "\n";
+
+}
+
+int main() {
+	// studyStrtodF();
+	studyNumericLimitsF();
 
 	return 0;
 }
