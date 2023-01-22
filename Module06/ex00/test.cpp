f696388db446d369fe929b7aae7108ad7f6886bb
pearpearB <smilyoung@gmail.com>
2023-01-22 16:57:31 +0900

    [Study] numeric_limits 공부 및 예제

--- Module06/ex00/test.cpp
+++ Module06/ex00/test.cpp
@@ -10,12 +10,25 @@ int main() {
 	float	f = static_cast<float>(d1);
 	std::cout << f << std::endl;
 
-	double	d2 = std::strtod("42.42abc", NULL);
+	double	d2 = std::strtod("-1", NULL);
 	std::cout << d2 << std::endl;
 
 	double	d3 = std::strtod("inf", NULL);  // check inf;
 	std::cout << d3 << std::endl;
 	std::cout << (d3 == std::numeric_limits<double>::infinity() ? "true" : "false") << std::endl;
+
+	std::cout << std::numeric_limits<double>::max() << "\n" 
+	<< std::numeric_limits<double>::min() << "\n" // 중간값, 0
+	<< std::numeric_limits<double>::lowest() << std::endl;
+
+
+	std::cout << std::numeric_limits<float>::max() << "\n" 
+	<< std::numeric_limits<float>::min() << "\n" // 중간값, 0
+	<< std::numeric_limits<float>::lowest() << std::endl;
+
+	std::cout << -std::numeric_limits<float>::max() << "\n";
+
+	std::cout << static_cast<int>(-std::numeric_limits<float>::max()) << "\n";
 
 	return 0;
 }
