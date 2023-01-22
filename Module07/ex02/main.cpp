efaf2a2956256501e93900f853e244e0e8dddd4f
pearpearB <smilyoung@gmail.com>
2023-01-22 20:26:28 +0900

    [Feat] 예외처리 예제 추가

--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -1,5 +1,4 @@
 #include "Array.hpp"
-#include <iostream>
 
 int main() {
 	Array<int> a(5);
@@ -7,12 +6,20 @@ int main() {
 		a[i] = i;
 	for (unsigned int i = 0; i < a.size(); i++)
 		std::cout << a[i] << std::endl;
+	
 	Array<int> b(a);
 	for (unsigned int i = 0; i < b.size(); i++)
 		std::cout << b[i] << std::endl;
+	
 	Array<int> c;
 	c = b;
 	for (unsigned int i = 0; i < c.size(); i++)
 		std::cout << c[i] << std::endl;
+	
+	try {
+		std::cout << c[9] << std::endl;
+	} catch (std::exception &e) {
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
 	return 0;
 }
