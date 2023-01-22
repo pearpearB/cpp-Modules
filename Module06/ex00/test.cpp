e4e4afc197f66076039fc805151ec723a4cab9f9
pearpearB <smilyoung@gmail.com>
2023-01-22 17:47:09 +0900

    [Study] lowest, -max true 확인

--- Module06/ex00/test.cpp
+++ Module06/ex00/test.cpp
@@ -3,32 +3,33 @@
 int main() {
 	char	*end;
 
+	std::cout << "\n===== study strtod & tset nan =====" << std::endl;
 	double	d1 = std::strtod("nanQQQ", &end); // check nan;
-	std::cout << d1 << std::endl;
-	std::cout << end << std::endl;
+	std::cout << "return value: " << d1 << std::endl;
+	std::cout << "두번째 인자 포인터: " << end << std::endl;
 
 	float	f = static_cast<float>(d1);
-	std::cout << f << std::endl;
+	std::cout << "casting float: " << f << std::endl;
 
+	std::cout << "\n===== test negative =====" << std::endl;
 	double	d2 = std::strtod("-1", NULL);
-	std::cout << d2 << std::endl;
+	std::cout << "return value: " << d2 << std::endl;
 
+	std::cout << "\n===== test inf =====" << std::endl;
 	double	d3 = std::strtod("inf", NULL);  // check inf;
-	std::cout << d3 << std::endl;
-	std::cout << (d3 == std::numeric_limits<double>::infinity() ? "true" : "false") << std::endl;
+	std::cout << "return value: " << d3 << std::endl;
+	std::cout << "compare infinity(): " << (d3 == std::numeric_limits<double>::infinity() ? "true" : "false") << std::endl;
 
-	std::cout << std::numeric_limits<double>::max() << "\n" 
-	<< std::numeric_limits<double>::min() << "\n" // 중간값, 0
-	<< std::numeric_limits<double>::lowest() << std::endl;
+	std::cout << "\n===== study numeric_limits =====" << std::endl;
+	std::cout << "max: " << std::numeric_limits<double>::max() << "\n" 
+	<< "min: " << std::numeric_limits<double>::min() << "\n" // 중간값, 0
+	<< "lowest: " << std::numeric_limits<double>::lowest() << std::endl;
+	std::cout << "compare lowest & -max(): " << (std::numeric_limits<double>::lowest() == -std::numeric_limits<double>::max() ? "true" : "false") << std::endl;
 
-
-	std::cout << std::numeric_limits<float>::max() << "\n" 
-	<< std::numeric_limits<float>::min() << "\n" // 중간값, 0
-	<< std::numeric_limits<float>::lowest() << std::endl;
-
-	std::cout << -std::numeric_limits<float>::max() << "\n";
-
-	std::cout << static_cast<int>(-std::numeric_limits<float>::max()) << "\n";
+	std::cout << "max: " << std::numeric_limits<float>::max() << "\n" 
+	<< "min: " << std::numeric_limits<float>::min() << "\n" // 중간값, 0
+	<< "lowest: " << std::numeric_limits<float>::lowest() << std::endl;
+	std::cout << "compare lowest & -max(): " << (std::numeric_limits<float>::lowest() == -std::numeric_limits<float>::max() ? "true" : "false") << std::endl;
 
 	return 0;
 }
