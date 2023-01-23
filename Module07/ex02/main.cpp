b372c1f51577c524a652bf5b3f8dff499dd5deac
pearpearB <smilyoung@gmail.com>
2023-01-23 18:04:24 +0900

    [Fix] 릭 체크할 수 있도록 수정 및 깊은 복사 확인

--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -1,9 +1,11 @@
 #include "Array.hpp"
 
-int main() {
+void testLeak() {
+	srand(time(NULL));
+
 	Array<int> a(5);
 	for (unsigned int i = 0; i < a.size(); i++)
-		a[i] = i;
+		a[i] = rand() % 100;
 	for (unsigned int i = 0; i < a.size(); i++)
 		std::cout << a[i] << std::endl;
 	
@@ -15,11 +17,23 @@ int main() {
 	c = b;
 	for (unsigned int i = 0; i < c.size(); i++)
 		std::cout << c[i] << std::endl;
+
+	std::cout << "test deep copy" << std::endl;
+	a[0] = 0;
+	std::cout << a[0] << ", " << b[0] << ", " << c[0] << std::endl;
 	
 	try {
-		std::cout << c[9] << std::endl;
+		std::cout << c[100] << std::endl;
 	} catch (std::exception &e) {
 		std::cout << "Exception: " << e.what() << std::endl;
 	}
+}
+
+int main() {
+
+	testLeak();
+
+	system("leaks array");
+
 	return 0;
 }
